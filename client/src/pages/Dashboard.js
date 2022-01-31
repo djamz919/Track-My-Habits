3d46c756dbf8e55521ae24564be3467ff683737d
daniel natale <dnatale667@gmail.com>
2022-01-31 13:44:39 -0500

    sorry to add another branch

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -3,8 +3,9 @@ import HabitTracker from '../components/HabitTracker';
 import HabitOptions from '../components/HabitOptions';
 import NewHabitForm from '../components/NewHabitForm';
 import Auth from '../utils/auth';
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_HABITS, QUERY_ME } from '../utils/queries';
+import { ADD_HABIT } from '../utils/mutations';
 
 
 const Dashboard = () => {
@@ -12,15 +13,13 @@ const Dashboard = () => {
     const { data: userData } = useQuery(QUERY_ME);
     const habitInfo = userData?.me.habits || [];
 
-    // const habitInfo = [
-    //     {
-    //         id: 1,
-    //         habitText: "Test habit",
-    //         createdAt: "1/29/2022",
-    //         username: "Testuser",
-    //         days: "1"
-    //     }
-    // ]
+
+  const [habitText, setHabitText] = useState('');
+const [addHabit, {error}] = useMutation(ADD_HABIT);
+//consider the code below when setting daysCount
+
+  const [characterCount, setCharacterCount] = useState(0);
+
 
     function UpdateHabit() {
         // console.log(loading);
@@ -28,7 +27,28 @@ const Dashboard = () => {
         console.log(habitInfo);
         console.log(userData);
     }
+    const handleChange = (event) => {
+        if (event.target.value.length <= 280) {
+          setHabitText(event.target.value);
+          setCharacterCount(event.target.value.length);
+        }
+      };
 
+      const handleFormSubmit = async event => {
+          event.preventDefault();
+      
+        //   console.log(habitInfo);
+        // console.log(userData);
+        try{
+            await addHabit({
+variables: {habitText}
+            });
+            setHabitText('');
+            setCharacterCount(0);
+        } catch(e){
+            console.log(e);
+        }
+      }
 
     return (
         <main className='dashboard'>
@@ -52,6 +72,24 @@ const Dashboard = () => {
                     <button className='habit-button' onClick={UpdateHabit}>
                         Update habit information
                     </button>
+                    <div className='card-body'>
+                <h5>You don't have any habits yet!</h5>
+                <form onSubmit={handleFormSubmit}>
+                    <input
+                        className="form-input"
+                        placeholder="Enter your new habit here!"
+                        name="habit"
+                        type="habit"
+                        id="habit"
+                        value={habitText}
+                        onChange={handleChange}
+                    />
+                    <button className="button" type="submit">
+                        Get Started!
+                    </button>
+                </form>
+                {/* {error && <div>Error</div>} */}
+            </div>
                 </>
             ) : (
                 <>
