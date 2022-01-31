7c9632b3a04860dd551aa0ad8f556bc54b05054e
jeffb <jeffb@bsdiweb.com>
2022-01-31 17:11:14 -0500

    Added id to days model

--- server/models/Day.js
+++ server/models/Day.js
@@ -2,6 +2,12 @@ const { Schema } = require('mongoose');
 
 const daySchema = new Schema(
     {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
         day: {
             type: Number,
             required: true,
