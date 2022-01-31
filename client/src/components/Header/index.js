66e6dc8fb37d8fdcb04b34f76ce2908347d7088e
jeffb <jeffb@bsdiweb.com>
2022-01-31 16:43:04 -0500

    Added jwt-decode and fixed header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -18,16 +18,18 @@ const Header = () => {
             </div>
             {Auth.loggedIn() ? (
                 <>
-                    <p>You're logged in</p>
-                    <a href="/" onClick={logout}>
-                        Logout
-                    </a>
+                    <div className='header-options'>
+                        <p>You're logged in</p>
+                        <Link to="/login" onClick={logout}>Logout</Link>
+                    </div>
                 </>
             ) : (
                 <>
-                    <p>You're logged out</p>
-                    <Link to="/login">Login</Link>
-                    <Link to="/signup">Signup</Link>
+                    <div className='header-options'>
+                        <p>You're logged out</p>
+                        <Link to="/login">Login</Link>
+                        <Link to="/signup">Signup</Link>
+                    </div>
                 </>
             )}
         </header >
